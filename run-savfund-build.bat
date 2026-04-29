43263cb736b5e9eee5fcdefa1c2d12e92f98bb33
Ashton Couture <ceo@centurionfinancial.com>
2026-04-28 21:12:48 -0500

    Deploy test build v3.0.8 [resend] - commandcenter sha 5c2ba46 2026-04-29 (clean)

--- run-savfund-build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo === SAVFUND VITE BUILD === > "%~dp0.logs\savfund-rebuild.txt" 2>&1
-cd /d "%~dp0savfund"
-call node_modules\.bin\vite.cmd build >> "%~dp0.logs\savfund-rebuild.txt" 2>&1
-if %ERRORLEVEL% EQU 0 (echo SAVFUND: PASS >> "%~dp0.logs\savfund-rebuild.txt") else (echo SAVFUND: FAIL >> "%~dp0.logs\savfund-rebuild.txt")
-echo === DONE === >> "%~dp0.logs\savfund-rebuild.txt"
